906ed5821589a1a38068d47a63c459b4c325fda2
Katharina Rychnavska <katharina.rychnavska@gmail.com>
2020-05-30 13:53:38 +0200

    Intial revision

---
+++ Giraffe Tutorial.py
@@ -0,0 +1,114 @@
+print("Hello World!")
+
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+character_name = "Kim"
+character_age = 54
+is_male = False
+
+print("There was a person named " + character_name + ",")
+# str(eine Nummer) dient der Umwandlung einer Nummer in eine Zahl
+print("She was " + str(character_age) + " years old.")
+print("She really liked the name " + character_name + ",")
+print("but didn't like being " + str(character_age) + ".")
+print(character_age)
+
+print(character_name)
+
+character_name = "George"
+character_age = 34
+is_male = True
+
+print("There was a man named " + character_name + ",")
+# str(eine Nummer) dient der Umwandlung einer Nummer in eine Zahl
+print("he was " + str(character_age) + " years old.")
+print("He really liked the name " + character_name + ",")
+print("but didn't like being " + str(character_age) + ".")
+
+# Das ist ein Kommentar
+# Das ist auch ein Kommentar
+# Das ist Kommentar #3
+
+print("Giraffe")
+
+print("Giraffe\nAcademy")
+
+# Zitate
+
+# \"Zitat\"
+print("\"Learn, learn, learn\", a famous man said.")
+
+# 'Text \"Zitat\" Text'
+print("'I said \"Hi\" to him'.")
+
+phrase = "Giraffe Academy"
+print(phrase)
+
+phrase = "My name is Katharina"
+print(phrase)
+
+phrase1 = "Summer"
+phrase2 = "Winter"
+
+print(phrase1)
+print(phrase2)
+
+print(phrase1 + " " + phrase2)
+
+phrase = "SPRING"
+print(phrase.lower())
+print(phrase.upper())
+# Functions kombinieren
+print(phrase.upper().isupper())
+
+phrase = "Giraffe Academy"
+print(phrase)
+print(phrase + " is cool")
+phrase = "Babyfitness"
+print(phrase + " is nice for moms with little babies.")
+
+# The length function
+print(len(phrase))
+# Print out the first letter of the phrase
+print(phrase[0])
+
+# The replace function
+phrase4 = "Be happy"
+print(phrase4.replace("happy", "glorious"))
+
+phrase5 = "I love chocolate"
+print(phrase5.replace("chocolate", "icecream"))
+
+phrase6 = "The sun is shining"
+print(phrase6.replace("sun", "moon"))
+
+# Numbers
+print(2)
+print(5 + 3)
+print(3 * 3)
+print(2.5 * 2)
+print(4.78)
+print(3 * 4 + 5)
+print(3 * (4 + 5))
+print(10 % 3)
+print(13 % 3)
+
+my_num = 28
+print(my_num)
+
+# How to convert a number into a string - to print out numbers alongside strings - a string function
+# Because you can't add a number to a string'
+my_num = 14
+print(str(my_num))
+
+my_num2 = 8
+print(str(my_num2) + " is my favourite number")
+
+# A comment needs space after #
+# There are further math functions like abs, min, max, round, floor, ceil, sqrt ...
+# You can import further math functions/external code into our file
+
+# Getting input from users
